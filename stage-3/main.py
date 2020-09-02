bf6ed6f9b7b1d6c166746d59f002cfd8e83c876c
alexioannides <alex.ioanndies@yahoo.co.uk>
2020-09-02 16:20:38 +0100

    Fix import error

--- stage-3/main.py
+++ stage-3/main.py
@@ -1,14 +1,14 @@
 """
 This module defines what will happen in stage-3.
 """
-from flask import Flask, make_request, jsonify
+from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    return make_request(jsonify({'y': 'hello_world'}))
+    return make_response(jsonify({'y': 'hello_world'}))
 
 
 if __name__ == '__main__':
