452d778b47ddad3c4e9a2f2439f0c7f0c9258551
alexioannides <alex.ioanndies@yahoo.co.uk>
2020-09-10 15:19:19 +0100

    Add secrets to stage-4 and add it to DAG

--- stage-4/main.py
+++ stage-4/main.py
@@ -1,6 +1,8 @@
 """
 This module defines what will happen in stage-3.
 """
+import os
+import sys
 from flask import Flask, make_response, jsonify
 
 app = Flask(__name__)
@@ -12,4 +14,13 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    try:
+        username = os.environ['USERNAME']
+        password = os.environ['PASSWORD']
+        print(f'username: {username}')
+        print(f'password: {password}')
+
+        app.run(host='0.0.0.0', port=5000)
+    except KeyError as e:
+        print(f'cannot find environment variable {e}')
+        sys.exit(1)
