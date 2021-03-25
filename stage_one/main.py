d5a179ca7b92db5b6520a31226e38ac6877bfe5a
alexioannides <alex.ioanndies@yahoo.co.uk>
2021-03-25 20:33:01 +0000

    Add args parameters to config

--- stage_one/main.py
+++ stage_one/main.py
@@ -1,12 +1,19 @@
 """
 This module defines what will happen in stage_1.
 """
+import sys
 from datetime import datetime
 
 
 def main() -> None:
     now = datetime.now()
     print(f'now={now}')
+    try:
+        sys.argv[1]
+        sys.argv[2]
+        sys.exit(0)
+    except Exception:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
